981e5cc0f5cdb964a7e15281dfda1a1b3afafd12
Asmar Siddiqui <60093530@cna-qatar.edu.qa>
2021-03-11 16:33:14 +0300

    added to db with sample data on register

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
-import firebase from './fb'
-import fetch from 'node-fetch'
-const db = firebase.firestore()
+import firebase from "./fb";
+import fetch from "node-fetch";
+const db = firebase.firestore();
 
 // const a = async () => {
 //     const response = await fetch('https://10.0.2.2:8080',
@@ -17,120 +17,202 @@ const db = firebase.firestore()
 
 // all database functionality here
 class DB {
-
     constructor(collection) {
-        this.collection = collection
+        this.collection = collection;
     }
 
     reformat(doc) {
-        console.log('reformat', doc.id)
-        return { id: doc.id, ...doc.data() }
+        console.log("reformat", doc.id);
+        return { id: doc.id, ...doc.data() };
     }
 
     findAll = async () => {
-        const data = await db.collection(this.collection).get()
-        return data.docs.map(this.reformat)
-    }
+        const data = await db.collection(this.collection).get();
+        return data.docs.map(this.reformat);
+    };
 
-    listenAll = set =>
-        db.collection(this.collection).onSnapshot(snap => set(snap.docs.map(this.reformat)))
+    listenAll = (set) =>
+        db.collection(this.collection).onSnapshot((snap) => set(snap.docs.map(this.reformat)));
 
-    findOne = async id => {
-        const doc = await db.collection(this.collection).doc(id).get()
-        return doc.exists ? this.reformat(doc) : undefined
-    }
+    findOne = async (id) => {
+        const doc = await db.collection(this.collection).doc(id).get();
+        return doc.exists ? this.reformat(doc) : undefined;
+    };
 
     listenOne = (set, id) =>
         id === ""
-            ?
-            set(null)
-            :
-            db.collection(this.collection).doc(id).onSnapshot(snap => set(this.reformat(snap)))
+            ? set(null)
+            : db
+                  .collection(this.collection)
+                  .doc(id)
+                  .onSnapshot((snap) => set(this.reformat(snap)));
 
     // item has no id
-    create = async item => {
-        const { id, ...rest } = item
-        return await db.collection(this.collection).add(rest)
-    }
+    create = async (item) => {
+        const { id, ...rest } = item;
+        return await db.collection(this.collection).add(rest);
+    };
 
     // item has id
-    update = async item => {
-        const { id, ...rest } = item
-        await db.collection(this.collection).doc(id).set(rest)
-    }
+    update = async (item) => {
+        const { id, ...rest } = item;
+        await db.collection(this.collection).doc(id).set(rest);
+    };
 
-    remove = async id => {
-        await db.collection(this.collection).doc(id).delete()
-    }
+    remove = async (id) => {
+        await db.collection(this.collection).doc(id).delete();
+    };
 }
 
-
 class Sensors extends DB {
-
     constructor() {
-        super('sensors')
-        this.Readings = new Readings(this.collection)
+        super("sensors");
+        this.Readings = new Readings(this.collection);
     }
 
     listenByCategory = (set, categoryid) =>
-        db.collection(this.collection).where("categoryid", "==", categoryid).onSnapshot(snap => set(snap.docs.map(this.reformat)))
+        db
+            .collection(this.collection)
+            .where("categoryid", "==", categoryid)
+            .onSnapshot((snap) => set(snap.docs.map(this.reformat)));
 
     listenByUser = (set, userid) =>
-        db.collection(this.collection).where("userid", "==", userid).onSnapshot(snap => set(snap.docs.map(this.reformat)))
+        db
+            .collection(this.collection)
+            .where("userid", "==", userid)
+            .onSnapshot((snap) => set(snap.docs.map(this.reformat)));
 
     listenByUserAndCategory = (set, userid, categoryid) =>
-        db.collection(this.collection).where("userid", "==", userid).where("categoryid", "==", categoryid).onSnapshot(snap => set(snap.docs.map(this.reformat)))
+        db
+            .collection(this.collection)
+            .where("userid", "==", userid)
+            .where("categoryid", "==", categoryid)
+            .onSnapshot((snap) => set(snap.docs.map(this.reformat)));
 
-    toggleMotionDetected = sensor =>
-        db.collection(this.collection).doc(sensor.id).set({ motiondetected: !sensor.motiondetected }, { merge: true })
+    toggleMotionDetected = (sensor) =>
+        db
+            .collection(this.collection)
+            .doc(sensor.id)
+            .set({ motiondetected: !sensor.motiondetected }, { merge: true });
 
     setMotionDetected = (sensor, motiondetected) =>
-        db.collection(this.collection).doc(sensor.id).set({ motiondetected }, { merge: true })
+        db.collection(this.collection).doc(sensor.id).set({ motiondetected }, { merge: true });
 
-    toggleAlert = sensor =>
-        db.collection(this.collection).doc(sensor.id).set({ alert: !sensor.alert }, { merge: true })
+    toggleAlert = (sensor) =>
+        db
+            .collection(this.collection)
+            .doc(sensor.id)
+            .set({ alert: !sensor.alert }, { merge: true });
 }
 
 class Readings extends DB {
-
     constructor(containing) {
-        super('readings')
-        this.containing = containing
+        super("readings");
+        this.containing = containing;
     }
 
     createReading = (sensorId, reading) =>
-        db.collection(this.containing).doc(sensorId).collection(this.collection).add(reading)
+        db.collection(this.containing).doc(sensorId).collection(this.collection).add(reading);
 
     listen2OrderByWhen = (set, sensorId) =>
-        db.collection(this.containing).doc(sensorId).collection(this.collection).orderBy("when", "desc").limit(2).onSnapshot(snap => set(snap.docs.map(this.reformat)))
+        db
+            .collection(this.containing)
+            .doc(sensorId)
+            .collection(this.collection)
+            .orderBy("when", "desc")
+            .limit(2)
+            .onSnapshot((snap) => set(snap.docs.map(this.reformat)));
 
     listenLatestOne = (set, sensorId) =>
-        db.collection(this.containing).doc(sensorId).collection(this.collection).orderBy("when", "desc").limit(1).onSnapshot(snap => set(snap.docs.map(this.reformat)[0]))
-
+        db
+            .collection(this.containing)
+            .doc(sensorId)
+            .collection(this.collection)
+            .orderBy("when", "desc")
+            .limit(1)
+            .onSnapshot((snap) => set(snap.docs.map(this.reformat)[0]));
 }
 
 class Users extends DB {
-
     constructor() {
-        super('users')
+        super("users");
+        this.Notifications = new Notifications(this.collection);
+        this.Gifts = new Gifts(this.collection);
     }
-
 }
 
 class Categories extends DB {
-
     constructor() {
-        super('categories')
+        super("categories");
     }
 
     // max 10
     listenInIds = (set, ids) =>
-        db.collection(this.collection).where(db.FieldPath.documentId(), "in", ids).onSnapshot(snap => set(snap.docs.map(this.reformat)))
-
+        db
+            .collection(this.collection)
+            .where(db.FieldPath.documentId(), "in", ids)
+            .onSnapshot((snap) => set(snap.docs.map(this.reformat)));
 }
+
+//Asmar Start
+
+class Notifications extends DB {
+    constructor(containing) {
+        super("notifications");
+        this.containing = containing;
+    }
+
+    send = async (userId, title, body, link, sensorId = undefined) => {
+        return await db
+            .collection(this.containing)
+            .doc(userId)
+            .collection(this.collection)
+            .add({
+                title,
+                body,
+                link,
+                isRead: false,
+                timestamp: new Date(),
+                ...(sensorId && { sensorId }),
+            });
+    };
+}
+
+class Advertisements extends DB {
+    constructor() {
+        super("advertisements");
+    }
+}
+
+class Gifts extends DB {
+    constructor(containing) {
+        super("gifts");
+        this.containing = containing;
+    }
+
+    create = async (userId, item) => {
+        return await db
+            .collection(this.containing)
+            .doc(userId)
+            .collection(this.collection)
+            .add(item);
+    };
+}
+
+class Promotions extends DB {
+    constructor() {
+        super("promotions");
+    }
+}
+//Asmar End
 
 export default {
     Categories: new Categories(),
     Sensors: new Sensors(),
-    Users: new Users()
-}
+    Users: new Users(),
+
+    //Asmar Start
+    Advertisements: new Advertisements(),
+    Promotions: new Promotions(),
+    //Asmar End
+};
